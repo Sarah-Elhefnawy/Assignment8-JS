1c491222294711a74d057ac614bec16dfc8a6881
Sarah <sarahelhefnawy0@gmail.com>
2025-05-14 22:26:11 +0300

    Final commit

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 var h2 = document.getElementById("demo");
 var h3 = document.getElementById("demo3");
 
-// Start of user entering a number then print it
+// 1 - Start of user entering a number then print it
 // var num1 = window.prompt("Enter a Number to be printed: ");
 // console.log(num1);
 // h2.innerHTML= num1;
-// End of user entering a number then print it
+// 1 - End of user entering a number then print it
 
-// Start of number divided by 3 and 4 otherwise print no
+// 2 - Start of number divided by 3 and 4 otherwise print no
 // var num2 = Number(window.prompt("Enter a Number divided by 3 & 4: "));
 // if (num2 % 3 === 0 && num2 % 4 === 0) {
 //     h2.innerHTML = "YES";
@@ -15,19 +15,23 @@ var h3 = document.getElementById("demo3");
 // else {
 //     h2.innerHTML = "NO";
 // }
-// End of number divided by 3 and 4 otherwise print no
+// 2 - End of number divided by 3 and 4 otherwise print no
 
-// Start of print the max between 2 integers
+// 3 - Start of print the max between 2 integers
 // var num3 = Number(window.prompt("Enter number 1(print the max): "));
 // var numm3 = Number(window.prompt("Enter number 2(print the max): "));
 // if (num3 > numm3) {
 //     h2.innerHTML = num3;
-// } else {
+// } 
+// else if (numm3 > num3){
 //     h2.innerHTML = numm3;
 // }
-// End of print the max between 2 integers
+// else {
+//     h2.innerHTML = "They both are equal numbers";
+// }
+// 3 - End of print the max between 2 integers
 
-// Start of number negative otherwise print positive
+// 4 - Start of number negative otherwise print positive
 // var num4 = Number(window.prompt("Enter a number to see its sign: "));
 // if (num4 > 0) {
 //     h2.innerHTML = "This number is Positive";
@@ -36,9 +40,9 @@ var h3 = document.getElementById("demo3");
 // } else {
 //     h2.innerHTML = "This number is Zero";
 // }
-// End of number negative otherwise print positive
+// 4 - End of number negative otherwise print positive
 
-// Start of 3 integers and print the max element and the min element
+// 5 - Start of 3 integers and print the max element and the min element
 // var num5 = Number(window.prompt("Enter the First number(print the max): "));
 // var numm5 = Number(window.prompt("Enter the Second number(print the max): "));
 // var nummm5 = Number(window.prompt("Enter the Third number(print the max): "));
@@ -64,51 +68,45 @@ var h3 = document.getElementById("demo3");
 //         h3.innerHTML = `The min number between ${num5}, ${numm5}, ${nummm5} is ${num5}`;
 //     }
 // }
-// End of 3 integers and print the max element and the min element
+// 5 - End of 3 integers and print the max element and the min element
 
-// Start of check If a number is oven or odd
+// 7 - Start of check If a number is oven or odd
 // var num7 = Number(window.prompt("Enter a number to see its even/odd: "));
 // if (num7 % 2 == 0) {
 //     h2.innerHTML = "This number is Even";
 // } else {
 //     h2.innerHTML = "This number is Odd";
 // }
-// End of check If a number is oven or odd
+// 7 - End of check If a number is oven or odd
 
-// Start of if it is vowel chars (a,e,I,o,u) then print vowel otherwise print consonant
+// 8 - Start of if it is vowel chars (a,e,I,o,u) then print vowel otherwise print consonant
 // var char8 = window.prompt("Enter any character to see if its (a,e,I,o,u): ").toLowerCase();
-// switch (char8) {
-//     case 'a':
-//     case 'e':
-//     case 'i':
-//     case 'u':
-//     case 'o':
-//         h2.innerHTML = "Vowel";
-//         break;
-//     default:
-//         h2.innerHTML = "Consonant";
-//         break;
+// if (char8 == 'a' || char8 == 'e' || char8 == 'i' || char8 == 'o' || char8 == 'u') {
+//     h2.innerHTML = "Vowel";
 // }
-// End of if it is vowel chars (a,e,I,o,u) then print vowel otherwise print consonant
+// else {
+//     h2.innerHTML = "Consonant";
+// }
+// 8 - End of if it is vowel chars (a,e,I,o,u) then print vowel otherwise print consonant
 
-// Start of print all numbers between 1 to that’s number
+// 9 - Start of print all numbers between 1 to that’s number
 // var num9 = Number(window.prompt("Enter any number printed to 10: "));
 // for (let index = 1; index <= num9; index++) {
 //     h2.innerHTML += index;
 //     console.log(index);
 // }
-// End of print all numbers between 1 to that’s number
+// 9 - End of print all numbers between 1 to that’s number
 
-// Start of print a multiplication table up to 12
+// 10 - Start of print a multiplication table up to 12
 // var num10 = Number(window.prompt("Enter a number to be multiblied till 12: "));
 // for (let index = 1; index <= 12; index++) {
 //     var result = num10 * index;
 //     console.log(result);
 //     h2.innerHTML += result;
 // }
-// End of print a multiplication table up to 12
+// 10 - End of print a multiplication table up to 12
 
-// Start of print all even numbers between 1 to this number
+// 11 - Start of print all even numbers between 1 to this number
 // var num11 = Number(window.prompt("Enter a number to print all its even numbers: "));
 // for (let index = 1; index <= num11; index++) {
 //     if (index % 2 ==0) {
@@ -116,9 +114,9 @@ var h3 = document.getElementById("demo3");
 //         h2.innerHTML += index;
 //     }
 // }
-// End of print all even numbers between 1 to this number
+// 11 - End of print all even numbers between 1 to this number
 
-// Start of print the power of two integers
+// 12 - Start of print the power of two integers
 // var num12 = Number(window.prompt("Enter the First number(print the power): "));
 // var numm12 = Number(window.prompt("Enter the Second number(print the power): "));
 // var result = 1;
@@ -130,9 +128,9 @@ var h3 = document.getElementById("demo3");
 // } else {
 //     console.log("Can't be zero");
 // }
-// End of print the power of two integers
+// 12 - End of print the power of two integers
 
-// Start of five subjects and calculate total, average and percentage
+// 12 - Start of five subjects and calculate total, average and percentage
 // var num12_1 = Number(window.prompt("Enter the First grade: "));
 // var num12_2 = Number(window.prompt("Enter the Second grade: "));
 // var num12_3 = Number(window.prompt("Enter the Third grade: "));
@@ -144,11 +142,65 @@ var h3 = document.getElementById("demo3");
 // console.log(`Total marks is ${totalGrades}`);
 // console.log(`Average is ${averageGrades}`);
 // console.log(`Percentage is ${percentageGrades}`);
-// End of five subjects and calculate total, average and percentage
+// 12 - End of five subjects and calculate total, average and percentage
 
-// Start of print number of days in that month
+// 13 - Start of print number of days in that month
 // var num13 = Number(window.prompt("Enter month number: "));
-// switch (num13) {
+// if (num13 == 1 || num13 == 3 || num13 == 5 || num13 == 7 || num13 == 8 || num13 == 10 || num13 == 12) {
+//             h2.innerHTML = "Days in Months: 31";
+// }
+// else if (num13 == 4 || num13 == 6 || num13 == 9 || num13 == 11) {
+//     h2.innerHTML = "Days in Months: 30";
+// }
+// else if (num13 == 2) {
+//     h2.innerHTML = "Days in Months: 28";
+// }
+// else {
+//     h2.innerHTML = "Months are from 1 to 12 only";
+// }
+// 13 - End of print number of days in that month
+
+// 14 - Start of marks of five subjects + Find percentage and grade
+// var percentage14 = Number(window.prompt("Enter your percentage(To get the mark): "));
+// switch (true) {
+//     case percentage14>=90:
+//         console.log("Your grade is A");
+//         h2.innerHTML = "Your grade is A";
+//         break;
+//     case percentage14>=80:
+//         console.log("Your grade is B");
+//         h2.innerHTML = "Your grade is B";
+//         break;
+//     case percentage14>=70:
+//         console.log("Your grade is C");
+//         h2.innerHTML = "Your grade is C";
+//         break;
+//     case percentage14>=60:
+//         console.log("Your grade is D");
+//         h2.innerHTML = "Your grade is D";
+//         break;
+//     case percentage14>=40:
+//         console.log("Your grade is E");
+//         h2.innerHTML = "Your grade is E";
+//         break;
+//     case percentage14<40:
+//         console.log("Your grade is F");
+//         h2.innerHTML = "Your grade is F";
+//         break;
+//     default:
+//         console.log("not a number");
+//         h2.innerHTML = "not a number";
+//         break;
+// }
+// 14 - End of marks of five subjects + Find percentage and grade
+
+
+// ******************************** Using switch case*******************************
+
+
+// 15 - Start of print number of days in that month
+// var num15 = Number(window.prompt("Enter month number: "));
+// switch (num15) {
 //     case 1:
 //     case 3:
 //     case 5:
@@ -156,16 +208,109 @@ var h3 = document.getElementById("demo3");
 //     case 8:
 //     case 10:
 //     case 12:
-//         h2.innerHTML = 31;
+//         h2.innerHTML = "Days in Months: 31";
 //         break;
 //     case 2:
-//         h2.innerHTML = 28;
+//         h2.innerHTML = "Days in Months: 28";
 //         break;
 //     case 4:
 //     case 6:
 //     case 9:
 //     case 11:
-//         h2.innerHTML = 30;
+//         h2.innerHTML = "Days in Months: 30";
 //         break;
 // }
-// End of print number of days in that month
+// 15 - End of print number of days in that month
+
+// 16 - Start of checking an alphabet is vowel or not
+// var char16 = window.prompt("Enter any character to see if its (a,e,I,o,u): ").toLowerCase();
+// switch (char16) {
+//     case 'a':
+//     case 'e':
+//     case 'i':
+//     case 'u':
+//     case 'o':
+//         h2.innerHTML = "Vowel";
+//         break;
+//     default:
+//         h2.innerHTML = "Consonant";
+//         break;
+// }
+// 16 - End if checking an alphabet is vowel or not
+
+// 17 - Start of finding maximum between two numbers
+// var num17 = Number(window.prompt("Enter First number(find the max): "));
+// var numm17 = Number(window.prompt("Enter First number(find the max): "));
+// switch (true) {
+//     case num17>numm17:
+//         h2.innerHTML = `The Maximum number is ${num17}`
+//         break;
+//     case numm17>num17:
+//         h2.innerHTML = `The Maximum number is ${numm17}`
+//         break;
+//     default:
+//         h2.innerHTML = "They both are equal numbers";
+//         break;
+// }
+// 17 - End of finding maximum between two numbers
+
+// 18 - Start of checking whether a number is even or odd
+// var num18 = Number(window.prompt("Enter any number(even/odd): "));
+// switch (true) {
+//     case num18%2==0:
+//         h2.innerHTML = `Number ${num18} is Even number`;
+//         break;
+//     case num18%2==1:
+//         h2.innerHTML = `Number ${num18} is Odd number`;
+//         break;
+//     default:
+//         h2.innerHTML = `${num18} is not a number`;
+//         break;
+// }
+// 18 - End of checking whether a number is even or odd
+
+// 19 - Start of number is positive or negative or zero
+// var num19 = Number(window.prompt("Enter any number(+/-/0): "));
+// switch (true) {
+//     case num19>0:
+//         h2.innerHTML = `Number ${num19} is Positive`;
+//         break;
+//     case num19<0:
+//         h2.innerHTML = `Number ${num19} is Negative`;
+//         break;
+//     case num19==0:
+//         h2.innerHTML = `Number ${num19} is Zero`;
+//         break;
+//     default:
+//         h2.innerHTML = `${num19} is not a number`;
+//         break;
+// }
+// 19 - End of number is positive or negative or zero
+
+// 20 - Start of Simple Calculator
+// var num20 = Number(window.prompt("Enter the First number: "));
+// var numm20 = Number(window.prompt("Enter the Second number: "));
+// var operator20 = window.prompt("Enter the Operator: ");
+// var result = "";
+// switch (operator20) {
+//     case '+':
+//         result = num20 + numm20;
+//         h2.innerHTML = `The Sum of ${num20} and ${numm20} is ${result}`;
+//         break;
+//     case '-':
+//         result = num20 - numm20;
+//         h2.innerHTML = `The Subtract of ${num20} and ${numm20} is ${result}`;
+//         break;
+//     case '*':
+//         result = num20 * numm20;
+//         h2.innerHTML = `The Multiby of ${num20} and ${numm20} is ${result}`;
+//         break;
+//     case '/':
+//         result = num20 / numm20;
+//         h2.innerHTML = `The Division of ${num20} and ${numm20} is ${result}`;
+//         break;
+//     default:
+//         h2.innerHTML = "Not an operator";
+//         break;
+// }
+// 20 - End of Simple Calculator
